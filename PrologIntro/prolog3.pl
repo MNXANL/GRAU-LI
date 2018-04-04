f694bcf24447b68eebfa7bc63e98d9c66fac7a73
Mmorenogomez <miguelmoregome@gmail.com>
2018-04-04 12:02:09 +0200

    99%

--- PrologIntro/prolog3.pl
+++ PrologIntro/prolog3.pl
@@ -7,10 +7,37 @@
 %%    ciclos que no contienen el uno para evitar la no-terminación del
 %%    programa.
 
-conv(1).
+numberToList(N, [N]):- N >= 0, 10 > N, !.
+numberToList(N, [X|L]):-
+    number(N), N >= 10,    N1 is N // 10,    X is N mod 10,
+    numberToList(N1, L),
+    true.
+
+numToList(N, L):- numberToList(N, Lr), reverse(Lr, L).
+
+
+sumSquare([], 0).
+sumSquare([X|L], N):-
+    sumSquare(L, N1),
+    N2 is X*X,
+    N is N1 + N2,
+    true.
+
 conv(N):-
-  N1 is N-1,
-  conv(N1).
+  numToList(N, L),
+  sumSquare(L, N1),
+  N1 >= 10,
+  conv(N1), !.
+  
+conv(N):-
+  numToList(N, L),
+  sumSquare(L, N1),
+  N1 = 1, !.
+  
+
+
+
+
 
 
 
@@ -27,7 +54,6 @@ conv(N):-
 %%     7 + La casa verde esta a la izquierda de la blanca
 %%     8 + El escultor cría caracoles
 %%     9 + El de la casa amarilla es actor
-
 %%    10 + El de la tercera casa bebe cava
 %%    11 + El que vive al lado del actor tiene un caballo
 %%    12 + El hungaro vive al lado de la casa azul
@@ -41,9 +67,7 @@ conv(N):-
 
 %% Ayuda: sigue el siguiente esquema:
 
-casaVerdeBlanca(Verde, Blanca):-
-  between(2, 5, Blanca),
-  Verde is Blanca-1.
+casaVerdeBlanca(Verde, Blanca):- vecinoLeft(Verde, Blanca).
 
 vecinos(A, B):- vecinoLeft(A, B).
 vecinos(A, B):- vecinoRight(A, B).
@@ -58,8 +82,9 @@ vecinoRight(A, B):-
   between(1, 4, B),
   A is B+1.
 
-casas:-	Sol = [	
-      [1,A1,B1,C1,D1,E1],
+casas:-	
+    Sol = [	
+        [1,A1,B1,C1,D1,E1],
   		[2,A2,B2,C2,D2,E2],
   		[3,A3,B3,C3,D3,E3],
   		[4,A4,B4,C4,D4,E4],
@@ -67,22 +92,23 @@ casas:-	Sol = [
     ],
     
     % member(  [1, A, B, C, D, E] , Sol),
-    member(  [_, roja,     _,        _,       _,      peru] ,    Sol),
-    member(  [_, _,        _,        perro,   _,      francia] , Sol),
-    member(  [_, _,        pintor,   _,       _,      japon] ,   Sol),
-    member(  [_, _,        _,        _,       ron,    china] ,   Sol),
-    member(  [1, _,        _,        _,       _,      hungria] , Sol),
-    member(  [G, verde,    _,        _,       cognac, _] ,       Sol),
-    member(  [W, blanca,   _,        _,       _,      _] ,       Sol), casaVerdeBlanca(G, W),
-    member(  [_, _,        escultor, caracol, _,      _] ,       Sol),
-    member(  [X, amarilla, actor,    _,       _,      _] ,       Sol),
-    member(  [3, _,        _,        _,       cava,   _] ,       Sol),
-    member(  [Y, _,        _,        caballo, _,      _] ,       Sol), vecinos(X, Y),
-    member(  [2, azul,     _,        _,       _,      _] ,       Sol),
-    member(  [_, _,        notario,  _,       whisky, _] ,       Sol),
-    member(  [A, _,        medico,   _,       _,      _] ,       Sol),
+    member( [_, roja,     _,        _,       _,      peru] ,    Sol),
+    member( [_, _,        _,        perro,   _,      francia] , Sol),
+    member( [_, _,        pintor,   _,       _,      japon] ,   Sol),
+    member( [_, _,        _,        _,       ron,    china] ,   Sol),
+    member( [1, _,        _,        _,       _,      hungria] , Sol),
+    member( [G, verde,    _,        _,       cognac, _] ,       Sol),
+    member( [W, blanca,   _,        _,       _,      _] ,       Sol), casaVerdeBlanca(G, W),
+    member( [_, _,        escultor, caracol, _,      _] ,       Sol),
+    member( [X, amarilla, actor,    _,       _,      _] ,       Sol),
+    member( [3, _,        _,        _,       cava,   _] ,       Sol),
+    member( [Y, _,        _,        caballo, _,      _] ,       Sol), vecinos(X, Y),
+    member( [2, azul,     _,        _,       _,      _] ,       Sol),
+    member( [_, _,        notario,  _,       whisky, _] ,       Sol),
+    member( [A, _,        medico,   _,       _,      _] ,       Sol),
     member(  [B, _,        _,        ardilla, _,      _] ,       Sol), vecinos(A, B),
-	write(Sol), nl.
+	
+    write(Sol), nl,!.
 
 
 %% 3. Haz un programa prolog que escriba la manera de colocar sobre un tablero de
@@ -97,4 +123,29 @@ casas:-	Sol = [
 %%       . . . . x . . .
 %%       . . . . . . x .
 %%       x . . . . . . .
+%%      [8,4,1,3,6,2,7,5]
 
+cabeza([Fst], Fst):- !.
+cabeza(Lst, Fst):- append([Fst], _, Lst), !.
+
+ultimo([Last], Last):- !.
+ultimo(List, Last) :-
+    append(_, [Last], List), !.
+
+notAttack(L):-
+    between(2, 8, Len),
+    append(Lx, Partition, Ly), append(Ly, Lz, L),
+    length(Lx, A), length(Lz, B), length(Partition, Len),
+    write(Partition), nl,
+    8 is A+Len+B,
+    cabeza(Partition, X), ultimo(Partition, Y),
+    L1 is Len-1,
+    L1 \= abs(X-Y),
+    write(L),
+    fail.
+
+
+reinas:-
+    permutation([1,2,3,4,5,6,7,8], L),
+    notAttack(L),
+    fail.
