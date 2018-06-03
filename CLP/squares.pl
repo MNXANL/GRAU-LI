8d486834cc691afba73273ec0990fa2228bc8920
MNXANL <miguelmoregome@gmail.com>
2018-06-03 16:55:30 +0200

    tomo remade

--- CLP/squares.pl
+++ CLP/squares.pl
@@ -1,22 +1,50 @@
 :- use_module(library(clpfd)).
 
 %ejemplo(_, Big, [S1...SN]): how to fit all squares of sizes S1...SN in a square of size Big?
-ejemplo(0,  3,[2,1,1,1,1,1]).
-ejemplo(1,  4,[2,2,2,1,1,1,1]).
-ejemplo(2,  5,[3,2,2,2,1,1,1,1]).
-ejemplo(3, 19,[10,9,7,6,4,4,3,3,3,3,3,2,2,2,1,1,1,1,1,1]).
-ejemplo(4,112,[50,42,37,35,33,29,27,25,24,19,18,17,16,15,11,9,8,7,6,4,2]).
-ejemplo(5,175,[81,64,56,55,51,43,39,38,35,33,31,30,29,20,18,16,14,9,8,5,4,3,2,1]).
+ejemplo(0,  3, [2,1,1,1,1,1]).
+ejemplo(1,  4, [2,2,2,1,1,1,1]).
+ejemplo(2,  5, [3,2,2,2,1,1,1,1]).
+ejemplo(3, 19, [10,9,7,6,4,4,3,3,3,3,3,2,2,2,1,1,1,1,1,1]).
+ejemplo(4,112, [50,42,37,35,33,29,27,25,24,19,18,17,16,15,11,9,8,7,6,4,2]).
+ejemplo(5,175, [81,64,56,55,51,43,39,38,35,33,31,30,29,20,18,16,14,9,8,5,4,3,2,1]).
+
+
+
+
+
+insideBigSquare(_, Big, [S|Sides], [V|Vars]):- 
+    (S+V) #=< (Big+1),    
+    insideBigSquare(_, Big, Sides, Vars).
+insideBigSquare(_, _, [], []).
+
+nonoverlapping(N, Sides, RowVars, ColVars):- 
+
+    true.
 
 main:- 
-    ejemplo(3,Big,Sides),
-    nl, write('Fitting all squares of size '), write(Sides), write(' into big square of size '), write(Big), nl,nl,
-    length(Sides,N), 
-    length(RowVars,N), % get list of N prolog vars: Row coordinates of each small square
-    insideBigSquare(N,Big,Sides,RowVars),
-    insideBigSquare(N,Big,Sides,ColVars),
-    nonoverlapping(N,Sides,RowVars,ColVars),
-    ...
+    %between(1, 3, Nex), 
+    %nl,nl,write('###################################################'),
+    %nl, write('#################### Ejemplo '), write(Nex), write(' ####################'), 
+    %nl, write('###################################################'), nl,
+    %nl,
+    ejemplo(0, Big, Sides),
+    nl, write('Fitting all squares of size '), write(Sides), 
+    write(' into big square of size '), write(Big), nl,nl,
+    
+    % Domains
+    length(Sides, N),   % get list of N prolog vars:
+    length(RowVars, N), %  -> Row coordinates of each small square
+    length(ColVars, N), %  -> Col coordinates of each small square
+    RowVars ins 1..Big,
+    ColVars ins 1..Big,
+    
+    % Constraints
+    insideBigSquare(N, Big, Sides, RowVars),
+    insideBigSquare(N, Big, Sides, ColVars),
+    nonoverlapping(N, Sides, RowVars, ColVars),
+    
+    % Labeling and output
+    append(RowVars, ColVars, ALL), labeling([ff], ALL),
     displaySol(N,Sides,RowVars,ColVars), halt.
 
 
--- CLP/tomografia.pl
+++ CLP/tomografia.pl
@@ -18,11 +18,22 @@
 %	 0                         
 %	
 
-:- use_module(library(clpfd)).
+/* POSSIBLE SOLUTION FOR THIS ONE
+      3  2  3  1  1  1  1  2  3  2  1 
 
-ejemplo1( [0,0,8,2,6,4,5,3,7,0,0], [0,0,7,1,6,3,4,5,2,7,0,0] ).
-ejemplo2( [10,4,8,5,6], [5,3,4,0,5,0,5,2,2,0,1,5,1] ).
-ejemplo3( [11,5,4], [3,2,3,1,1,1,1,2,3,2,1] ).
+ 11   *  *  *  *  *  *  *  *  *  *  *  
+  5   *     *              *  *  *     
+  4   *  *  *                 *        
+
+  
+*/
+
+
+:-use_module(library(clpfd)).
+
+ejemplo(1, [0,0,8,2,6,4,5,3,7,0,0], [0,0,7,1,6,3,4,5,2,7,0,0] ).
+ejemplo(2, [10,4,8,5,6], [5,3,4,0,5,0,5,2,2,0,1,5,1] ).
+ejemplo(3, [11,5,4], [3,2,3,1,1,1,1,2,3,2,1] ).
 
 
 
@@ -35,31 +46,54 @@ matrixByRows(List, NC, [Col|Cols]):-
 	matrixByRows(Residual, NC, Cols),
 	true.
 	
-declareConstraints([C|MC], [R|MR], [Cx|CS], [Rx|RS], ColSums):-
-	X in 0..1,
-	sum([A,B,C], #=, R),
-	declareConstraints(MC, RS, ColSums)
+% Constraint -> get as many dots as Sum
+getDots([Line|Matrix], [Sum|MoreSums]):-
+	sum(Line, #=, Sum),
+	getDots(Matrix, MoreSums),
 	true.
+getDots([], _).
 
 
-p:-	ejemplo3(RowSums,ColSums),
+% MOD: it throws every possible case for every example!
+p:-	
+    between(1, 3, N), 
+    nl,nl,write('###################################################'),
+    nl, write('#################### Ejemplo '), write(N), write(' ####################'), 
+    nl, write('###################################################'), nl,
+    nl,
+    ejemplo(N, RowSums,ColSums),
 	length(RowSums,NumRows),
 	length(ColSums,NumCols),
 	NVars is NumRows*NumCols,
 	listVars(NVars,L),  % generate a list of Prolog vars (their names do not matter)
+	
+	% Domain and variables
 	L ins 0..1,
-	%...
 	matrixByRows(L,NumCols,MatrixByRows),
-	write('MBR\n'), write(MatrixByRows), nl, 
-	%transpose(MatrixByRows, MatrixByCols),
+	transpose(MatrixByRows, MatrixByCols),
 	
-	declareConstraints(MatrixByCols, RowSums, ColSums),
+	% Constraints
+    getDots(MatrixByRows, RowSums),
+	getDots(MatrixByCols, ColSums),
 	
-	pretty_print(RowSums,ColSums,MatrixByRows).
+	% Output and labeling
+	label(L), nl,nl,
+	pretty_print(RowSums,ColSums,MatrixByRows), fail.
 
 
-pretty_print(_,ColSums,_):- write('     '), member(S,ColSums), writef('%2r ',[S]), fail.
-pretty_print(RowSums,_,M):- nl,nth1(N,M,Row), nth1(N,RowSums,S), nl, writef('%3r   ',[S]), member(B,Row), wbit(B), fail.
+pretty_print(_,ColSums,_):- 
+    write('     '), 
+    member(S,ColSums), 
+    writef('%2r ',[S]), 
+    fail.
+pretty_print(RowSums,_,M):- 
+    nl, nth1(N,M,Row), 
+    nth1(N,RowSums,S), nl, 
+    writef('%3r   ',[S]), 
+    member(B,Row), wbit(B), 
+    fail.
 pretty_print(_,_,_).
+
 wbit(1):- write('*  '),!.
 wbit(0):- write('   '),!.
+
